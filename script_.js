cf4f935afa859c52751727d99c4dbce4701e6c9c
codingsake <thinthazin.chris@gmail.com>
2022-01-17 00:06:01 -0500

    adding comment on js

--- script_.js
+++ script_.js
@@ -1,5 +1,6 @@
+//initialize the count to 0.
 var count = 0;
-var btn = document.getElementById("btn");
+//store display id in disp variable
 var disp = document.getElementById("display");
 
 function changeHeart() {
@@ -12,11 +13,13 @@ function changeHeart() {
   if (heartId.src.match("heart-regular.svg")) {
       // if src of heart img is regular then change to solid
       heartId.src = "heart-solid.svg";
+      //set the count to one and print when the heart change to solid
       var count = 1;
       disp.innerHTML = count;
   } else {
       // change back to regular
       heartId.src = "heart-regular.svg";
+      //set the count to 0 and print when the heart change to regular
       var count = 0;
       disp.innerHTML = count;
     }
